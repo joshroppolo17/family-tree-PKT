cf619d94221bafca3831bda05094fb6b96b1b59a
joshroppolo17 <joshroppolo17@gmail.com>
2025-11-03 13:33:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 /* global vis, tinycolor, brothers, $, didYouMean */
 
+// Mock out dependencies for testing on NodeJS. These are imported in HTML in
+// the browser.
 /* eslint-disable */
 /* istanbul ignore else */
 if (typeof brothers === 'undefined') {
   brothers = require('./relations');
 }
+/* istanbul ignore else */
 if (typeof tinycolor === 'undefined') {
   tinycolor = require('tinycolor2');
 }
+/* istanbul ignore else */
 if (typeof $ === 'undefined') {
   $ = require('jquery');
 }
+/* istanbul ignore else */
 if (typeof vis === 'undefined') {
   vis = require('vis');
 }
+/* istanbul ignore else */
 if (typeof didYouMean === 'undefined') {
   didYouMean = require('didyoumean');
 }
@@ -27,7 +33,11 @@ var nodesDataSet;
 var edgesDataSet;
 var previousSearchFind;
 
-var DIRECTION = { FORWARD: 0, BACKWARD: 1 };
+var DIRECTION = {
+  FORWARD: 0,
+  BACKWARD: 1,
+};
+
 var KEYCODE_ENTER = 13;
 var familyColorGlobal = {};
 var pledgeClassColorGlobal = {};
@@ -43,12 +53,16 @@ ColorSpinner.prototype.spin = function () {
 
 var getNewFamilyColor = (function () {
   var spinner1 = new ColorSpinner({ h: 0, s: 0.6, v: 0.9 }, 77);
-  return function () { return spinner1.spin(); };
+  return function () {
+    return spinner1.spin();
+  };
 }());
 
 var getNewPledgeClassColor = (function () {
   var spinner2 = new ColorSpinner({ h: 0, s: 0.4, v: 0.9 }, 23);
-  return function () { return spinner2.spin(); };
+  return function () {
+    return spinner2.spin();
+  };
 }());
 
 function didYouMeanWrapper(invalidName) {
@@ -69,8 +83,8 @@ function createNodes(brothers_) {
   for (var i = 0; i < oldLength; i++) {
     var bro = brothers_[i];
     bro.id = i;
-
     var lowerCaseFamily = (bro.familystarted || '').toLowerCase();
+
     if (lowerCaseFamily && !familyColor[lowerCaseFamily]) {
       familyColor[lowerCaseFamily] = getNewFamilyColor();
       var newNode = {
@@ -91,17 +105,18 @@ function createNodes(brothers_) {
         id: newIdx++,
         name: '',
         label: '[' + bro.name + ']',
-        family: lowerCaseFamily,
+        family: bro.familystarted.toLowerCase(),
       }));
       var familyNode = familyToNode[lowerCaseFamily];
       edges.push({ from: familyNode.id, to: bro.id });
     } else if (!bro.big && !lowerCaseFamily) {
-      throw new Error('Little bro without a big bro: ' + bro.name);
+      throw new Error('Encountered a little bro (' + bro.name + ') without a big bro.');
     } else if (lowerCaseFamily) {
       edges.push({ from: familyToNode[lowerCaseFamily].id, to: bro.id });
     } else {
       edges.push({ from: bro.big, to: bro.id });
     }
+
     bro.big = bro.big || lowerCaseFamily;
 
     var lowerCaseClass = (bro.pledgeclass || '').toLowerCase();
@@ -134,9 +149,7 @@ function createNodes(brothers_) {
       var node = nameToNode[edge.from];
       if (!node) {
         var correctedName = didYouMeanWrapper(edge.from);
-        var msg = correctedName
-          ? 'Did you mean ' + correctedName + '?'
-          : 'No match for ' + edge.from;
+        var msg = correctedName ? 'Did you mean ' + correctedName + '?' : 'No match for ' + edge.from;
         throw new Error(msg);
       }
       edge.from = node.id;
@@ -146,7 +159,7 @@ function createNodes(brothers_) {
   function getFamily(node) {
     node.family = node.family || node.familystarted;
     if (node.family) return node.family;
-    try { node.family = getFamily(node.big); } catch (e) { node.family = 'unknown'; }
+    try { node.family = getFamily(node.big); } catch(e) { node.family = 'unknown'; }
     return node.family;
   }
 
@@ -211,47 +224,57 @@ function draw() {
 
   var changeColor;
   var colorMethod = document.getElementById('layout').value;
-  switch (colorMethod) {
+  switch(colorMethod) {
     case 'active':
-      changeColor = function (node) {
+      changeColor = function(node) {
         node.color = (node.inactive || node.graduated) ? 'lightgrey' : 'lightblue';
         nodesDataSet.update(node);
-      };
-      break;
+      }; break;
     case 'pledgeClass':
-      changeColor = function (node) {
-        node.color = node.pledgeclass
-          ? pledgeClassColorGlobal[node.pledgeclass.toLowerCase()]
-          : 'lightgrey';
+      changeColor = function(node) {
+        node.color = node.pledgeclass ? pledgeClassColorGlobal[node.pledgeclass.toLowerCase()] : 'lightgrey';
         nodesDataSet.update(node);
-      };
-      break;
-    default:
-      changeColor = function (node) {
+      }; break;
+    default: // family
+      changeColor = function(node) {
         node.color = familyColorGlobal[node.family.toLowerCase()];
         nodesDataSet.update(node);
-      };
-      break;
+      }; break;
   }
+
   nodesGlobal.forEach(changeColor);
 
   if (!network) {
     var container = document.getElementById('mynetwork');
     var data = { nodes: nodesDataSet, edges: edgesDataSet };
+
     var options = {
-      layout: {
-        hierarchical: {
-          sortMethod: 'directed',
-          direction: 'UD',       // top-down
-          levelSeparation: 250,  // vertical spacing between families
-          nodeSpacing: 200       // horizontal spacing between nodes
-        },
+      nodes: {
+        shape: "ellipse",
+        font: { size: 16, multi: true },
+        margin: 12
       },
       edges: {
-        smooth: true,
-        arrows: { to: true },
+        arrows: "to",
+        smooth: false
       },
+      layout: {
+        hierarchical: {
+          direction: "UD", // top → down
+          sortMethod: "directed",
+          nodeSpacing: 350,
+          levelSeparation: 150 // smaller = higher up
+        }
+      },
+      physics: {
+        hierarchicalRepulsion: {
+          nodeDistance: 300,
+          springLength: 300
+        },
+        solver: "hierarchicalRepulsion"
+      }
     };
+
     network = new vis.Network(container, data, options);
   } else {
     network.redraw();
@@ -259,45 +282,42 @@ function draw() {
 }
 
 if (typeof document !== 'undefined') {
-  $(document).ready(function () {
+  $(document).ready(function() {
     draw();
+
     var dropdown = document.getElementById('layout');
-    dropdown.onchange = function () { draw(); };
+    dropdown.onchange = draw;
 
     function hidePrevNextButtons() {
-      $('#prevsearch').css('display', 'none');
-      $('#nextsearch').css('display', 'none');
+      $('#prevsearch, #nextsearch').hide();
     }
     function showPrevNextButtons() {
-      $('#prevsearch').css('display', 'inline');
-      $('#nextsearch').css('display', 'inline');
+      $('#prevsearch, #nextsearch').show();
     }
+
     function search(direction) {
-      if (direction !== DIRECTION.FORWARD && direction !== DIRECTION.BACKWARD) {
-        console.warn('Unexpected direction value: ' + direction + ' (defaulting to FORWARD)');
-        direction = DIRECTION.FORWARD;
-      }
-      direction = direction || DIRECTION.FORWARD;
+      direction = direction !== DIRECTION.FORWARD && direction !== DIRECTION.BACKWARD ? DIRECTION.FORWARD : direction;
       var query = $('#searchbox').val();
       var success = findBrotherHelper(query, direction);
-      if (success) {
-        $('#searchbox').css('background-color', 'white');
-        if (query !== '') showPrevNextButtons();
-        else hidePrevNextButtons();
+      if(success) {
+        $('#searchbox').css('background-color','white');
+        query ? showPrevNextButtons() : hidePrevNextButtons();
       } else {
-        $('#searchbox').css('background-color', '#EEC4C6');
+        $('#searchbox').css('background-color','#EEC4C6');
         hidePrevNextButtons();
       }
     }
-    document.getElementById('searchbox').onkeypress = function (e) {
-      if (!e) e = window.event;
+
+    document.getElementById('searchbox').onkeypress = function(e){
+      e = e || window.event;
       var keyCode = e.keyCode || e.which;
-      if (keyCode === KEYCODE_ENTER && !e.shiftKey) search(DIRECTION.FORWARD);
-      if (keyCode === KEYCODE_ENTER && e.shiftKey) search(DIRECTION.BACKWARD);
+      if(keyCode === KEYCODE_ENTER && !e.shiftKey) search(DIRECTION.FORWARD);
+      if(keyCode === KEYCODE_ENTER && e.shiftKey) search(DIRECTION.BACKWARD);
     };
-    document.getElementById('searchbutton').onclick = search.bind(undefined, DIRECTION.FORWARD);
-    document.getElementById('nextsearch').onclick = search.bind(undefined, DIRECTION.FORWARD);
-    document.getElementById('prevsearch').onclick = search.bind(undefined, DIRECTION.BACKWARD);
+
+    $('#searchbutton').click(search.bind(undefined, DIRECTION.FORWARD));
+    $('#nextsearch').click(search.bind(undefined, DIRECTION.FORWARD));
+    $('#prevsearch').click(search.bind(undefined, DIRECTION.BACKWARD));
   });
 }
 
